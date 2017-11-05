17b83e4d0666b7b09f27cda42345d9fd73c78a96
Late Worm <travis.rempel@gmail.com>
2017-11-05 09:26:35 -0800

    Change colours

--- src/partials/Board.js
+++ src/partials/Board.js
@@ -11,14 +11,16 @@ export default class Board {
 		let board = document.createElementNS(SVG_NS, 'rect');
 		board.setAttributeNS(null, 'width', this.width);
 		board.setAttributeNS(null, 'height', this.height);
-		board.setAttributeNS(null, 'fill', '#522');
+		board.setAttributeNS(null, 'fill', '#333');
+		board.setAttributeNS(null, 'rx', 3); // corner radius, horizontal
+		board.setAttributeNS(null, 'ry', 3); // corner radius, vertical
 
 		let net = document.createElementNS(SVG_NS, 'line')
 		net.setAttributeNS(null, 'x1', this.width/2)
 		net.setAttributeNS(null, 'y1', 0)
 		net.setAttributeNS(null, 'x2', this.width/2)
 		net.setAttributeNS(null, 'y2', this.height)
-		net.setAttributeNS(null, 'stroke', '#a66')
+		net.setAttributeNS(null, 'stroke', '#666')
 		net.setAttributeNS(null, 'stroke-opacity', '0.5')
 		net.setAttributeNS(null, 'stroke-width', '4')
 
