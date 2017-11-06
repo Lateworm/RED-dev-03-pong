c586cc18a87615b2f122ab11f6a85ad13b38e580
Late Worm <travis.rempel@gmail.com>
2017-11-05 17:49:31 -0800

    Change P2's keys so the game can be played in Firefox

--- src/settings.js
+++ src/settings.js
@@ -3,8 +3,8 @@ export const SVG_NS = 'http://www.w3.org/2000/svg';
 export const KEYS = {
   p1up: 		'a',    // player 1 up key
   p1down: 	'z',    // player 1 down key
-  p2up: 		'\'',   // player 2 up key
-  p2down: 	'/',    // player 2 down key
+  p2up: 		'k',   // player 2 up key
+  p2down: 	'm',    // player 2 down key
   spaceBar: ' ', 		// we'll use this later...
 }
 
